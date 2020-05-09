04b0268dfcab55d75a721487bba63f21f9045070
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-09 19:39:44 +0100

    Removed the list

--- lib/main.dart
+++ lib/main.dart
@@ -25,17 +25,6 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -73,14 +62,6 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
-                  );
-                });
               },
             ),
           ),
@@ -104,7 +85,7 @@ class _QuizPageState extends State<QuizPage> {
           ),
         ),
         Row(
-          children: scoreKeeper,
+          children: <Widget>[],
         ),
       ],
     );
