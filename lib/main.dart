9e88f9eb0696612afbe7f415048991dd6abe9e21
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-10 15:16:03 +0100

    Created a collections of questino objects

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,16 +27,18 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
+
   int questionNumber = 0;
-  List<bool> answers = [
-    false,
-    true,
-    true,
+
+  List<Question> questionBank = [
+    Question(
+        questionText: 'You can lead a cow down stairs but not up stairs.',
+        answer: false),
+    Question(
+        questionText:
+            'Approximately one quarter of human bones are in the feet.',
+        answer: true),
+    Question(questionText: 'A slug\'s blood is green.', answer: true),
   ];
 
   @override
